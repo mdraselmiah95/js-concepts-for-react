cfdbd0b6c291b2383252ac2b2941bede35914883
rasel <russelroy15@gmail.com>
2021-09-11 13:10:19 +0600

    new code added

--- fundamentals.js
+++ fundamentals.js
@@ -24,3 +24,26 @@ for (let i = 0; i < numbers.length; i++) {
   const number = numbers[i];
   console.log(number);
 }
+
+//5. function
+
+function multiply(num1, num2) {
+  const result = num1 * num2;
+  return result;
+}
+
+const output = multiply(23, 9);
+
+//6. object
+
+const student = {
+  name: "Shakib",
+  age: 54,
+  movies: ["not now", "how are you", "king khan"],
+};
+
+const myVariable = "age";
+
+console.log(student.age); //direct by property
+console.log(student["age"]); //access via property name string
+console.log(student[myVariable]); //access via property name in a variable
