f73dbb123eb575497e689ae5331d9ad708dd1570
rasel <russelroy15@gmail.com>
2021-09-11 13:31:12 +0600

    new es6 code added

--- es6.js
+++ es6.js
@@ -5,7 +5,18 @@ const student = {
   movies: ["not now", "how are you", "king khan"],
 };
 
-//template string
+//1.template string
 const about = `
 My name is ${student.name} age of ${student.age} has number ${numbers[3]} also liked movies ${student.movies[2]}
 `;
+
+//2. arrow function
+
+const getSixtyTwo = () => 55;
+const addSixtyFive = (num) => num + 65;
+const isEven = (x) => x % 2 == 0;
+const addThree = (num1, num2, num3) => num1 + num2 + num3;
+const doMath = (x, y) => {
+  const sum = x + y;
+  return sum;
+};
