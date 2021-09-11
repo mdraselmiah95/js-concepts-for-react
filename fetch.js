a9719bb5f474971d16e310888c156e2705a66c4d
rasel <russelroy15@gmail.com>
2021-09-11 15:55:05 +0600

    learning for each and es6

--- fetch.js
+++ fetch.js
@@ -11,3 +11,23 @@ console.log(studentJSON);
 
 const studentObj = JSON.parse(studentJSON);
 console.log(studentObj);
+
+//fetch
+
+// fetch("url")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+//keys ,values
+
+const keys = Object.keys(student);
+const values = Object.values(student);
+
+//for Each
+
+const numbers = [23, 65, 88, 12, 99, 12, 5];
+numbers.forEach((num) => console.log(num));
+numbers.map((num) => num * 2);
+
+//for of on array like object
+//loop on an object using for in
