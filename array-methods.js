3861574ca4085b70facc27986324650e87729b62
rasel <russelroy15@gmail.com>
2021-09-11 14:04:41 +0600

    new es6 code added

--- array-methods.js
+++ array-methods.js
@@ -6,7 +6,22 @@ const products = [
   { name: "camera", price: 37990, brand: "canon", color: "sliver" },
   { name: "Tv", price: 50000, brand: "LG", color: "black" },
   { name: "fan", price: 3190, brand: "walton", color: "white" },
+  { name: "laptop", price: 310990, brand: "apple", color: "black" },
 ];
 
 const brands = products.map((product) => product.brand);
-console.log(brands);
+const price = products.map((x) => x.price);
+// console.log(price);
+// console.log(brands);
+
+products.forEach((product) => console.log(product));
+products.forEach((product) => console.log(product.color));
+
+products.forEach((product) => {});
+
+//filter
+const cheap = products.filter((product) => product.price <= 12000);
+console.log(cheap);
+
+const specificName = products.filter((product) => product.name.includes("la"));
+console.log(specificName);
