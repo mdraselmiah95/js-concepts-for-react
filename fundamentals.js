e5b25c2fdcd870bfc5d40e991b3ce0ebbaebbe95
rasel <russelroy15@gmail.com>
2021-09-11 13:01:23 +0600

    new code added

--- fundamentals.js
+++ fundamentals.js
@@ -1,1 +1,13 @@
 // 1. How to declare a variable using let and const
+
+const fatherName = "abc";
+let season = "rainy";
+season = "winter";
+
+// six basic condition >,< ,===,!==,<=,>=
+//multiple condition : &&, ||
+
+if (fatherName === "abc" || season === "rainy") {
+} else if (fatherName === "roni") {
+} else {
+}
