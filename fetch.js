2fa8bd670a683f98bd145a949f35967e5816bcb2
rasel <russelroy15@gmail.com>
2021-09-11 16:03:35 +0600

    learning for each and es6

--- fetch.js
+++ fetch.js
@@ -31,3 +31,29 @@ numbers.map((num) => num * 2);
 
 //for of on array like object
 //loop on an object using for in
+
+//add or remove from an array
+
+const products = [
+  { name: "laptop", price: 31990, brand: "HP", color: "black" },
+  { name: "phone", price: 12000, brand: "Realme", color: "Green" },
+  { name: "watch", price: 6000, brand: "casio", color: "pink" },
+  { name: "sunglass", price: 990, brand: "Ray-bon", color: "black" },
+  { name: "camera", price: 37990, brand: "canon", color: "sliver" },
+  { name: "Tv", price: 50000, brand: "LG", color: "black" },
+  { name: "fan", price: 3190, brand: "walton", color: "white" },
+  { name: "laptop", price: 310990, brand: "apple", color: "black" },
+];
+
+const newProduct = { name: "webcam", price: 499, brand: "Red" };
+
+// copy products array and then add newProduct
+
+const newProducts = [...products, newProduct];
+// console.log(newProducts);
+
+// create a new array without one specific item
+// remove phone means create a new array without the phone
+
+const remaining = products.filter((p) => p.name !== "laptop");
+console.log(remaining);
