02b6087065a2ff775494140461aacd3ac62dc0ca
rasel <russelroy15@gmail.com>
2021-09-11 15:46:41 +0600

    learning JSON

---
+++ fetch.js
@@ -0,0 +1,10 @@
+//JSON
+const student = {
+  name: "Shakib",
+  age: 54,
+  movies: ["not now", "how are you", "king khan"],
+};
+
+const studentJSON = JSON.stringify(student);
+console.log(student);
+console.log(studentJSON);
