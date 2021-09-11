a82a1297fbaf7c93f8b0c591434c0cf1a589f8c2
rasel <russelroy15@gmail.com>
2021-09-11 15:34:34 +0600

    new es6 code added

--- destructuring.js
+++ destructuring.js
@@ -3,7 +3,22 @@
 const numbers = [12, 93];
 const x = numbers[0];
 const y = numbers[1];
-console.log(x);
-console.log(y);
-const [x, y] = [12, 93];
-const [x, y] = numbers;
+// console.log(x);
+// console.log(y);
+// const [x, y] = [12, 93];
+// const [x, y] = numbers;
+
+function box(num1, num2) {
+  const numbers = [num1, num2];
+  return numbers;
+}
+
+// console.log(box(40, 99));
+
+const student = {
+  name: "Shakib",
+  age: 54,
+  movies: ["inception", "happiness", "gravity", "accountant"],
+};
+
+const [firstMovie, secondMovie, ThirdMovie, forthMovie] = student.movies;
