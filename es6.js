3338aba2fb9183dfa9b1dfae7de6a2813fb72297
rasel <russelroy15@gmail.com>
2021-09-11 13:26:22 +0600

    new code added

---
+++ es6.js
@@ -0,0 +1,11 @@
+const numbers = [12, 55, 787, 89, 09, 23];
+const student = {
+  name: "Shakib",
+  age: 54,
+  movies: ["not now", "how are you", "king khan"],
+};
+
+//template string
+const about = `
+My name is ${student.name} age of ${student.age} has number ${numbers[3]} also liked movies ${student.movies[2]}
+`;
