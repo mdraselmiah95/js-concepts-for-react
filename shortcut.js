5f385e0193178bd11b745dd3ebcc43d6ae9cf7eb
rasel <russelroy15@gmail.com>
2021-09-11 16:15:10 +0600

    learning true and falsy

--- shortcut.js
+++ shortcut.js
@@ -1,2 +1,31 @@
-// 'almas', 5, true, {}, []
-// '', 0, false, null, undefined
+// 'asif', 5, true, {}, []  =truthy
+// '', 0, false, null, undefined  =falsy
+
+//check truthy
+let myVar = 5;
+//check any truthy
+if (myVar) {
+  myVar = myVar * 100;
+} else {
+  myVar = 0;
+}
+
+//you check negative or falsy anything
+
+let myMoney = 100;
+if (!myMoney) {
+}
+
+const money = 80;
+let food;
+if (money > 100) {
+  food = "Batter";
+} else {
+  food = "banana";
+}
+
+let foodOne = money > 100 ? "Honey" : "nut";
+console.log(foodOne);
+
+let drink = money > 100 && myVar > 100 ? "coke" : "normal water";
+console.log(drink);
